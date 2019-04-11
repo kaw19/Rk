7aeef1dbcbaa95293bdbb14c85133ed559081d8c
Cláudio A. Fleury <46454054+kaw19@users.noreply.github.com>
2019-04-11 13:07:29 -0300

    Add files via upload

---
+++ eco/eco.ino
@@ -0,0 +1,56 @@
+/* Sketch:   teste_MotoresDC_3.5
+   Objetivo: movimentar RK com comandos simples: mover(), virar()
+             e acionando atuadores: Buzzer e LED RGB (branco opaco, catodo [-] comum)
+   Por:      Prof. Cláudio
+   Em:       29.11.2018 - IFG - Campus Goiânia                    */
+
+#include "Rkmotor.h"       // comandos de movimentação do Rk
+#include "Rkbuzledultra.h" // comandos dos atuadores: Buzzer e LED RGB
+
+void setup() {
+  pinMode(TRIG,OUTPUT); pinMode(ECHO,INPUT);
+  pinMode(BUZ, OUTPUT); pinMode(VERM,OUTPUT); 
+  pinMode(AZUL,OUTPUT); pinMode(VERD,OUTPUT); 
+  digitalWrite(VERM,LOW); digitalWrite(VERD,LOW); 
+  digitalWrite(AZUL,LOW); digitalWrite(BUZ, LOW);
+  pinMode(MA1, OUTPUT); pinMode(MA2, OUTPUT);
+  pinMode(MB1, OUTPUT); pinMode(MB2, OUTPUT);
+  pinMode(IP1, INPUT_PULLUP); pinMode(IP2, INPUT_PULLUP);
+  pararMotor('*');                                                // desliga ambos motores
+  Serial.begin(9600);
+  Serial.println("RK - v.1.0 Nov/2018\n");
+  furosA = furosB = 0;
+  attachInterrupt(digitalPinToInterrupt(IP1), motorA, RISING);    // interrupção gerada pelo Encoder do motor 'A'
+  attachInterrupt(digitalPinToInterrupt(IP2), motorB, RISING);    // interrupção gerada pelo Encoder do motor 'B'
+  led(AZUL,LIGA);           // Passo B
+  apitar(0.1);              // Passo A
+}
+
+void loop() {
+//   infravermelho(DIREITO);
+//   delay(1000);
+//  led(VERM,LIGA);           // Passo B
+//  apitar(0.5);              // Passo A
+//  mover(40,NORMAL);
+//  led(VERM,DESLIGA);
+//  led(VERD,LIGA);           // Passo C
+//  virar(360);
+//  led(VERD,DESLIGA);
+//  led(AZUL,LIGA);           // Passo D
+//  mover(-20,NORMAL);
+//  led(AZUL,DESLIGA);
+//  apitar(0.5);              // Passo E
+//  fim();
+  
+  if (infravermelho(DIREITO) && infravermelho(ESQUERDO))
+    // led(VERM, LIGA);
+    avancar();
+  if (!infravermelho(DIREITO) && infravermelho(ESQUERDO))
+    direita();
+  if (infravermelho(DIREITO) && !infravermelho(ESQUERDO))
+    esquerda();
+  if (!infravermelho(DIREITO) && !infravermelho(ESQUERDO)) {   // encontrou linha transversal
+    pararMotor('*');
+    fim();
+  }
+}
